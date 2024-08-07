ab76d63b23a7782d2f4e2f349347966bbd754345
felipehsoliveira <felipe.hsoliveira@outlook.com>
2024-08-07 19:58:34 -0300

    add eslint validation

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,4 +1,4 @@
-import { test as setup, expect } from '@playwright/test'
+import { test as setup } from '@playwright/test'
 import { Login } from '../objects/comonPages.ts'
 import * as elements from '../data/elements.ts';
 
@@ -6,13 +6,13 @@ import * as elements from '../data/elements.ts';
 const authFile = 'playwright/.auth/user.json';
 
 setup('authenticate work', async ({ page }) => {
-    let login_class = new Login(page);    
+    const login_class = new Login(page);    
     await login_class.login(elements.login_users.standard_user, elements.login_users.password, elements.mapping.url_login);
     await page.waitForURL('https://www.saucedemo.com/inventory.html');
     await page.context().storageState({path: authFile })
 })
 
-const authFileError = 'playwright/.auth/userError.json';
+// const authFileError = 'playwright/.auth/userError.json';
 // setup('authenticate work', async ({ page }) => {
 //     let login_class: any = new Login(page);    
 //     await login_class.login(elements.login_users.standard_user, elements.login_users.password, elements.mapping.url);
